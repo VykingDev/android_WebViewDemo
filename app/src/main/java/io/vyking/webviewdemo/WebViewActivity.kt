0c2f8f373642d8691c7c8fb770de33c775a5fb71
stevebritton <steve@vyking.io>
2025-06-30 16:25:37 +0100

    Use the model-viewer's webView to pre-warm the Tensorflow model

--- app/src/main/java/io/vyking/webviewdemo/WebViewActivity.kt
+++ app/src/main/java/io/vyking/webviewdemo/WebViewActivity.kt
@@ -7,6 +7,7 @@ import android.net.http.SslError
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.webkit.*
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -16,14 +17,28 @@ import androidx.core.content.ContextCompat
 import io.vyking.webviewdemo.databinding.ActivityWebviewBinding
 import java.net.URL
 
+class WebViewInterface(private val activity: WebViewActivity, private val button: com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton) {
+    @JavascriptInterface
+    // A function that can be called when pre-warming the Tensorfow model completes
+    fun onPreWarmTensorflowModelComplete() {
+        // Now the pre-warm is complete we can enable the VTO button
+        activity.runOnUiThread {
+            button.apply {
+                setVisibility(View.VISIBLE)
+            }
+        }
+    }
+}
+
 class WebViewActivity : AppCompatActivity() {
     private val tag: (String) -> String = { it -> "${this.javaClass.name}.$it" }
 
-    private lateinit var binding: ActivityWebviewBinding
+    lateinit var binding: ActivityWebviewBinding
     private var vykingWebView: WebView? = null
 
     private val key = "io.vyking"
-    private val config = "../assets/config/modeld.foot.bin"
+    private val config = "https://sneaker-window.vyking.io/vyking-examples/vanilla/assets/config/modeld.foot.config"
+    private var isPreWarmOfTensorflowModelComplete: Boolean = false
 
     private val vykingApparelUrl = "https://sneaker-window.vyking.io/vyking-examples/with-service-worker/examples/in-app-vyking-apparel-camera.html"
     private val modelViewerUrl   = "https://sneaker-window.vyking.io/vyking-examples/with-service-worker/examples/in-app-model-viewer.html"
@@ -79,6 +94,10 @@ class WebViewActivity : AppCompatActivity() {
             }
 
             viewModeToggle.apply {
+                // Keep this button hidden until the pre-warming of the Tensorflow model has completed.
+                // This will occur during the presentation of the model-viewer WKWebView.
+                setVisibility(View.INVISIBLE)
+
                 setOnClickListener {
                     viewMode = when (viewMode) {
                         ViewMode.vykingApparel -> ViewMode.modelViewer
@@ -192,6 +211,10 @@ class WebViewActivity : AppCompatActivity() {
                 }
             }
 
+            // Add a function that can be called when pre-warming the Tensorfow model completes
+            val preWarmTensorflowModelCompletionInterface = WebViewInterface(this@WebViewActivity, binding.viewModeToggle)
+            this.addJavascriptInterface(preWarmTensorflowModelCompletionInterface, "NativeAndroid")
+
             try {
                 Log.d(tag("addVykingWebView"), "url. $modelViewerUrl")
 
@@ -235,14 +258,35 @@ class WebViewActivity : AppCompatActivity() {
 
     private fun vykingConfigure(config: String, key: String, resultCallback: ((String) -> Unit)?) {
         vykingWebView?.evaluateJavascript("""
-        document.querySelector('vyking-apparel')?.setAttribute('key-key', '${key}');
+        document.querySelector('vyking-apparel')?.setAttribute('config-key', '${key}');
         document.querySelector('vyking-apparel')?.setAttribute('config', '${config}');
-
-        document.querySelector('model-viewer')?.setAttribute('vto', true);
-        document.querySelector('model-viewer')?.setAttribute('vto-share', true);
-        document.querySelector('model-viewer')?.setAttribute('vto-key', '${key}');
-        document.querySelector('model-viewer')?.setAttribute('vto-config', '${config}');
       """.trimIndent(), resultCallback)
+
+        // It can take a long time to compile the Tensorflow model's shaders, therefore we provide a javascript that can be run to do this before
+        // the VTO is required. This typically only needs to be done the first time the app launched after installation and after a device restart,
+        // however for simplicity this demo app does this on each app start.
+        // It is assumed this completion handler has been called by a WebView displayed before the VTO WebView is needed.
+        if (!isPreWarmOfTensorflowModelComplete) {
+            vykingWebView?.evaluateJavascript("""
+            const preWarmTensorflowModel = (configUrl, configKey) => {
+              import('https://sneaker-window.vyking.io/vyking-apparel/1/preWarmTensorflowModel.js')
+                .then(module => {
+                  return module.preWarmTensorflowModel(configUrl, configKey)
+                .then(() => {
+                  console.log('Pre-warmed TensorFlow model')
+                })
+              })
+              .catch((error) => {
+                console.error('Error pre-warming TensorFlow model:', error)
+              })
+              .finally(() => {
+                  // Just because the pre-warming failed, it doesn't mean the VTO won't run
+                  NativeAndroid.onPreWarmTensorflowModelComplete()
+              })
+            }
+            preWarmTensorflowModel('${config}', '${key}')
+            """.trimIndent()) { value -> isPreWarmOfTensorflowModelComplete = true }
+        }
     }
 
     private fun vykingReplaceApparel(url: String, name: String, resultCallback: ((String) -> Unit)?) {
